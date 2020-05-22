1dedf9600ae4a603be89a48e8bede915afdcf44b
Luca Rossi <lrossi@exeter.eecs.qmul.ac.uk>
2020-05-22 17:26:40 +0100

    test.py

--- test.py
+++ test.py
@@ -3,27 +3,47 @@ import numpy as np
 import random as rn
 import kdegree as kd
 import time
+import sys
+import getopt
 
-G = nx.read_edgelist('test graphs/PT.txt',nodetype=int)
-#G = nx.relabel_nodes(G, lambda x: x-1)
-#G = nx.barabasi_albert_graph(1000,3)
-#G = nx.erdos_renyi_graph(1000,0.01)
-#G = nx.watts_strogatz_graph(1000,10,0.1)
-#G = nx.read_gpickle('test graphs/ws_test_1.gpkl')
+def main(argv):
+  
+  input_file = None
+  k = None
+  noise = None
 
-print(nx.number_of_nodes(G),nx.number_of_edges(G),nx.density(G))
+  try:
+    opts, args = getopt.getopt(argv,"hi:k:n:")
+  except getopt.GetoptError:
+    print('test.py -i <inputfile> -k <k> -n <noise>')
+    sys.exit(2)
+  for opt, arg in opts:
+    print(opt,arg) 
+    if opt == '-h':
+      print('test.py -i <inputfile> -k <k> -n <noise>')
+      sys.exit()
+    elif opt is "-i":
+      input_file = arg
+    elif opt is "-k":
+      k = arg
+    elif opt is "-n": 
+      noise = arg 
+  
+  
+  G = nx.read_edgelist(input_file,nodetype=int)
+  
+  start = time.time()
+  Ga = kd.graph_anonymiser(G,k=k,algorithm=2,noise=noise,greedy_anonymiser=False)
+  print("Total execution time =",time.time()-start)
 
-noise=10
-k=30
-start = time.time()
-Ga = kd.graph_anonymiser(G,k=k,algorithm=1,noise=noise,greedy_anonymiser=False)
-print("Total execution time =",time.time()-start)
+  H = nx.intersection(G,Ga)
 
-H = nx.intersection(G,Ga)
+  num_edges_in_G = len(set(G.edges()))
+  num_edges_in_both = len(set(H.edges()))
 
-num_edges_in_G = len(set(G.edges()))
-num_edges_in_both = len(set(H.edges()))
+  print("Edges overlap = " + str(100*num_edges_in_both/num_edges_in_G)+"%")
+  print("Num edges original graph = " + str(nx.number_of_edges(G)))
+  print("Num edges anonymised graph = " + str(nx.number_of_edges(Ga)))
 
-print("Edges overlap = " + str(100*num_edges_in_both/num_edges_in_G)+"%")
-print("Num edges original graph = " + str(nx.number_of_edges(G)))
-print("Num edges anonymised graph = " + str(nx.number_of_edges(Ga)))
+if __name__ == "__main__":
+   main(sys.argv[1:])
